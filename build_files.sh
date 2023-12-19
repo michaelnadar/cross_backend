986c300a3f235d0d88d3da28dfff1bd2fcac088e
Michealajit <michealajit1@gmail.com>
2023-12-19 18:45:40 +0000

    changed gigs

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 apt-get install -y libpq-dev
+pip install -r requirements.txt 
+python3.9 manage.py collectstatic
--- cross_backend/settings.py
+++ cross_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3+#5+6fv&pnq45c@!bl*qvk3gh4*t6x(jrjofpvas-a550-j*l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','localhost']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -133,6 +133,8 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
